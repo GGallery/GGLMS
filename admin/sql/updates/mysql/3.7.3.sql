331d540c70af204577c8cd7405889d8acb4a6338
AntonioGiangrave <antonio@ggallery.it>
2017-06-29 17:45:46 +0200

    clear sql 3.7.8

--- admin/sql/updates/mysql/3.7.3.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `#__gg_unit`
-ADD COLUMN `pubblicato`  int(10) NULL AFTER `tipologia`;
-
--- admin/sql/updates/mysql/3.7.6.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `#__gg_unit`
-DROP COLUMN `tipologia`,
-ADD COLUMN `is_corso`  int(10) NULL AFTER `unitapadre`,
-ADD COLUMN `id_contenuto_completamento`  int(10) NULL AFTER `id_event_booking`;
-
