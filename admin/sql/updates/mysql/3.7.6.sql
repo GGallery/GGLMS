3a1a90240035242dbf9b5cad232d1a54317ff73d
AntonioGiangrave <antonio@ggallery.it>
2017-06-29 16:38:25 +0200

    3.7.6

--- admin/sql/updates/mysql/3.7.6.sql
+++ admin/sql/updates/mysql/3.7.6.sql
@@ -3,10 +3,3 @@ DROP COLUMN `tipologia`,
 ADD COLUMN `is_corso`  int(10) NULL AFTER `unitapadre`,
 ADD COLUMN `id_contenuto_completamento`  int(10) NULL AFTER `id_event_booking`;
 
-CREATE TRIGGER `ProtectUnitPadre` AFTER UPDATE ON `jos_gg_unit`
-FOR EACH ROW         BEGIN
-           IF OLD.id = 1 THEN
-                SET NEW.unitapadre = 0 , NEW.id = 1, NEW.ordinamento = 0;
-           END IF;
-END;;
-
