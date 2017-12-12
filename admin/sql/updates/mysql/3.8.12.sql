81a4fd2297756d28bf2a6aa75f0d409d11627378
antoniopetruzzella <33827114+antoniopetruzzella@users.noreply.github.com>
2017-12-12 12:37:31 +0100

    Update 3.8.12.sql

--- admin/sql/updates/mysql/3.8.12.sql
+++ admin/sql/updates/mysql/3.8.12.sql
@@ -2,7 +2,7 @@ ALTER TABLE `#__gg_unit`
 ADD COLUMN `data_inizio` date NULL AFTER `is_corso`,
 ADD COLUMN `data_fine` date NULL AFTER `data_inizio`;
 
-CREATE TABLE `un_gg_csv_report`  (
+CREATE TABLE `#__gg_csv_report`  (
   `id_chiamata` int(255) NOT NULL,
   `id_utente` int(255) NOT NULL,
   `nome` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
