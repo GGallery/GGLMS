bfde4e5d69ed25ac96d0f3ef52b72c8c8c5e7f29
Tony <antonio@ggallery.it>
2017-12-14 10:33:10 +0100

    Correzione bug Aggiornamento controllo codice fiscale

--- site/controllers/gglms.php
+++ site/controllers/gglms.php
@@ -17,18 +17,60 @@ defined('_JEXEC') or die;
 class gglmsControllergglms extends JControllerLegacy
 {
 
-    public function updatecf(){
+    protected $_db;
+    private $_app;
 
-        $app = JFactory::getApplication();
-        $jinput = JFactory::getApplication()->input;
-        $getdata= $jinput->post;
+    public function __construct($config = array())
+    {
+        parent::__construct($config);
+        $this->_app = JFactory::getApplication();
+        $this->_db = JFactory::getDbo();
 
-        $cfcorretto = $getdata->get('cfcorretto');
-        $data= json_decode(base64_decode($getdata->get('data')));
 
-        $conformita = utilityHelper::conformita_cf($cfcorretto);
-        if($conformita['valido']) {
-            $db = JFactory::getDbo();
+    }
+
+    public function updatecf()
+    {
+        try {
+            $jinput = JFactory::getApplication()->input;
+            $getdata = $jinput->post;
+
+            $cfcorretto = $getdata->get('cfcorretto');
+            $data = json_decode(base64_decode($getdata->get('data')));
+
+            $conformita = utilityHelper::conformita_cf($cfcorretto);
+            if ($conformita['valido']) {
+                $this->_db = JFactory::getDbo();
+
+                switch ($data->integration) {
+                    case "eb":
+                        $this->update_cf_eb($cfcorretto, $data);
+                        break;
+                    case "cb":
+                        $this->update_cf_cb($cfcorretto, $data);
+                        break;
+                    default:
+                        die('integrazione non gestita');
+                        break;
+                }
+
+                $this->_app->enqueueMessage('Modifica effettuata correttamente');
+                $url = JRoute::_('index.php?option=com_gglms&view=contenuto&alias=' . $data->return, false);
+                $this->_app->redirect($url, 'Modifica effettuata correttamente');
+
+            } else {
+                $this->_app->enqueueMessage('Anche il nuovo codice fiscale (' . $cfcorretto . ') sembra avere problemi! ', 'error');
+                $this->_app->redirect(JRoute::_('index.php?option=com_gglms&view=gglms&layout=mcf&data=' . $getdata->get('data')));
+            }
+        }
+        catch (Exception $e){
+            DEBUGG::error($e, 'Error updatecf controller GGLMS ', 1);
+        }
+    }
+
+    private function update_cf_eb($cfcorretto, $data)
+    {
+        try {
             $query = "UPDATE `#__eb_field_values` as fv 
                   INNER JOIN #__eb_fields as f on f.id = fv.field_id
                   SET field_value ='" . $cfcorretto . "'
@@ -36,21 +78,29 @@ class gglmsControllergglms extends JControllerLegacy
                   AND registrant_id = " . $data->registrant_id . "
                   ";
 
-            $db->setQuery($query);
-            $db->execute();
-
-
-            $app->enqueueMessage('Modifica effettuata correttamente');
-
-            $url = JRoute::_('index.php?option=com_gglms&view=contenuto&alias='.$data->return,false);
-            $app->redirect($url, 'Modifica effettuata correttamente');
-
-
+            $this->_db->setQuery($query);
+            return $this->_db->execute();
         }
-        else{
-            $app->enqueueMessage('Anche il nuovo codice fiscale ('.$cfcorretto.') sembra avere problemi! ', 'error');
-            $app->redirect(JRoute::_('index.php?option=com_gglms&view=gglms&layout=mcf&data='.$getdata->get('data')));
+        catch (Exception $e){
+            DEBUGG::error($e, 'Error update_cf_eb controller GGLMS', 1);
         }
-
     }
+
+    private function update_cf_cb($cfcorretto, $data)
+    {
+        try {
+            $query = "UPDATE `#__comprofiler` as c 
+                  SET " . $data->field_id . " = '" . $cfcorretto . "'
+                  WHERE  user_id = " . $data->registrant_id . "
+                  ";
+
+            $this->_db->setQuery($query);
+            return $this->_db->execute();
+        }
+        catch (Exception $e){
+            DEBUGG::error($e, 'Error update_cf_cb controller GGLMS', 1);
+        }
+    }
+
+
 }
