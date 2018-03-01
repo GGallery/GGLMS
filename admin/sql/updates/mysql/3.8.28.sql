5d2645709166b40b22eba83c075b5338c23cf2b9
Tony <[tony@bslt.it]>
2018-03-01 15:36:01 +0100

    Aggiunta tipologia contenuto SOLOPDF

--- admin/sql/updates/mysql/3.8.28.sql
+++ admin/sql/updates/mysql/3.8.28.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `un_gg_view_stato_user_unita`
+ALTER TABLE `#__gg_view_stato_user_unita`
 ADD COLUMN `data_inizio`  date NULL AFTER `stato`,
 ADD COLUMN `data_fine`  date NULL AFTER `data_inizio`;
 
-ALTER TABLE `un_gg_view_stato_user_corso`
+ALTER TABLE `#__gg_view_stato_user_corso`
 ADD COLUMN `data_inizio`  date NULL AFTER `stato`,
 ADD COLUMN `data_fine`  date NULL AFTER `data_inizio`;
