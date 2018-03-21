dfc17b1e8283a7a4fe540caa5f9bc1dbbd0f90e3
Tony <[tony@bslt.it]>
2018-03-21 14:46:35 +0100

    Layout PDF Singolo

--- site/views/contenuto/tmpl/default_pdfsingolo.php
+++ site/views/contenuto/tmpl/default_pdfsingolo.php
@@ -10,10 +10,24 @@ if($this->contenuto->_params->get('abilita_breadcrumbs',1))
 
 <div class="g-grid">
     <div class="g-block size-50 center">
-        <a href="<?php echo PATH_CONTENUTI.'/'.$this->contenuto->id. '/'.$this->contenuto->id.'.pdf'; ?>"><img src="components/com_gglms/libraries/images/icona_pdf.png"></a>
+
+        <?php
+        if($this->contenuto->descrizione == '' && $this->contenuto->abstract == '')
+            echo $this->contenuto->_params->get('testo_invito_scaricare_pdfsingolo');
+        else
+        {
+            echo $this->contenuto->descrizione;
+            echo "<br>";
+            echo $this->contenuto->abstract;
+        }
+        ?>
+
     </div>
     <div class="g-block size-50 center">
-        <?php echo $this->contenuto->_params->get('testo_invito_scaricare_pdfsingolo');?>
+        <a target="_blank" href="<?php echo PATH_CONTENUTI.'/'.$this->contenuto->id. '/'.$this->contenuto->id.'.pdf'; ?>">
+            <img src="components/com_gglms/libraries/images/icona_pdf.png">
+        </a>
     </div>
+
 
 </div>
