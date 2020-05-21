090be22889034b7e65895e92ff8b17a781e954a4
Francesca <francesca.bagni@gmail.com>
2020-05-21 10:43:37 +0200

    catalogo prenota ok

--- site/views/catalogo/tmpl/catalogo_prenota.php
+++ site/views/catalogo/tmpl/catalogo_prenota.php
@@ -4,19 +4,17 @@
 defined('_JEXEC') or die('Restricted access');
 
 echo '<div class="main">'; ?>
-<h1> I nostri corsi </h1>
+<?php echo  JText::_('COM_GGLMS_CATALOGO_TITOLO') ?>
 <?php
 foreach ($this->catalogo as $item) { ?>
 
 
     <div class="item-container">
 
-        <!--        Non sono sicura che serva id piattaforma-->
-        <!--echo  $this->id_piattaforma -->
         <div>
             <h6> <?php echo $item->titolo ?> </h6>
             <a style="font-weight: bold" href="<?php echo JURI::root() . 'index.php?option=com_gglms&view=prenota&id_corso=' . $item->id_corso . '&id_piattaforma=' . $this->id_piattaforma ?>">
-                PREZZI E ACQUISTO >>
+                <?php echo  JText::_('COM_GGLMS_CATALOGO_INFO') ?>
             </a>
         </div>
 
