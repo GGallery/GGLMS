aaaa6d6b5b8f9b04afbbadf63adfc3270fd6b7c9
Francesca <francesca.bagni@gmail.com>
2020-05-21 10:18:50 +0200

    attestati bulk ok

--- site/views/attestatibulk/tmpl/default.php
+++ site/views/attestatibulk/tmpl/default.php
@@ -3,21 +3,20 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-
-echo "<h1>Scarica Attestati</h1>";
-
-
 ?>
+
+
+<h1><?php echo  JText::_('COM_GGLMS_ATTESTATI_BULK_TITOLO') ?></h1>
 <div>
     <form autocomplete="off" id="form-genera-coupon"
           method="post" name="downloadAttForm" id="dwlForm" style="padding: 20px 100px;" class="form-validate">
         <div class="form-group row">
-            <label class="col-sm-2" for="id_corso">Corso:</label>
+            <label class="col-sm-2" for="id_corso"><?php echo  JText::_('COM_GGLMS_GLOBAL_CORSO') ?>:</label>
             <div class="col-sm-10">
                 <select required placeholder="Corso" type="text" class="form-control cpn_opt"
                         id="id_corso" name="id_corso">
                     <option value="-1">
-                        Seleziona un corso
+                        <?php echo  JText::_('COM_GGLMS_GLOBAL_SCEGLI_CORSO') ?>
                     </option>
                     <?php foreach ($this->lista_corsi as $c) { ?>
                         <option value="<?php echo $c->value; ?>">
@@ -29,14 +28,14 @@ echo "<h1>Scarica Attestati</h1>";
             </div>
         </div>
         <div class="form-group row" id="calendar_startdate_div">
-            <label class="col-sm-2" for="startdate">Completato dal:</label>
+            <label class="col-sm-2" for="startdate"><?php echo  JText::_('COM_GGLMS_REPORT_COMPLETATI_FROM') ?></label>
             <div class="col-sm-10">
                 <?php echo JHTML::calendar('', 'startdate', 'startdate', '%Y-%m-%d', array('onChange' => '_scaricaattesati.getDays()')); ?>
             </div>
         </div>
 
         <div class="form-group row" id="calendar_finishdate_div">
-            <label class="col-sm-2" for="finishdate">Completato al:</label>
+            <label class="col-sm-2" for="finishdate"><?php echo  JText::_('COM_GGLMS_REPORT_COMPLETATI_TO') ?></label>
             <div class="col-sm-10">
                 <?php echo JHTML::calendar('NOW', 'finishdate', 'finishdate', '%Y-%m-%d', array('onChange' => '_scaricaattesati.getDays()')); ?>
             </div>
@@ -48,7 +47,7 @@ echo "<h1>Scarica Attestati</h1>";
 
         <div class="form-group" style="text-align: center">
             <a id="btn-download" style="padding: 2px 32px;" type="button" target="_blank" href=""
-               class="btn btn-lg disabled">SCARICA</a>
+               class="btn btn-lg disabled"><?php echo  JText::_('COM_GGLMS_GLOBAL_DOWNLOAD') ?></a>
         </div>
     </form>
 
