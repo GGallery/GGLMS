2172fc73ba49e6365166f6b494aeaba670a73497
Luca <luglab@gmail.com>
2020-12-30 16:22:54 +0100

    XLS: fix path

--- site/libraries/xls/phpspreadsheet.php
+++ site/libraries/xls/phpspreadsheet.php
@@ -1,4 +1,4 @@
 <?php
 defined( '_JEXEC' ) or die;
 //require_once JPATH_LIBRARIES . '/phpspreadsheet/vendor/autoload.php';
-require_once JPATH_LIBRARIES . './vendor/autoload.php';
+require_once JPATH_LIBRARIES . '/vendor/autoload.php';
