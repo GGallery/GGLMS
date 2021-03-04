24c07b69a4e3dd28e34c0e3abbf6e5a8c7061581
Luca <luglab@gmail.com>
2021-03-04 08:50:31 +0100

    Integrazione con API Zoom per lo scaricamento dei report meeting/webinar. Integrazione vendita con modulo di acquisto corsi / eventi; fix di vario genere

--- admin/sql/updates/mysql/4.0.8.sql
+++ admin/sql/updates/mysql/4.0.8.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `#__gg_zoom` (
+CREATE TABLE IF NOT EXISTS `#__gg_zoom` (
   `id`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `token`  text NOT NULL ,
   `data_registrazione`  datetime NOT NULL ,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `#__gg_zoom_events` (
+CREATE TABLE IF NOT EXISTS `#__gg_zoom_events` (
   `id`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `id_evento`  bigint(20) UNSIGNED NOT NULL,
   `tipo_evento`  varchar(50) NOT NULL ,
