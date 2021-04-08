496fe40e1782171ce9c8f8218d9f5d4695c16e7f
Luca <luglab@gmail.com>
2021-04-08 12:05:33 +0200

    Fix install script

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -469,9 +469,9 @@ CREATE TABLE `#__gg_unit` (
   `prezzo` decimal(6,2) DEFAULT NULL,
   `sc_a_data` date DEFAULT NULL,
   `sc_valore_data` decimal(6,2) DEFAULT NULL,
-  `sc_a_data_gruppi` text,
+  `sc_a_data_gruppi` text DEFAULT NULL,
   `sc_valore_data_gruppi` decimal(6,2) DEFAULT NULL,
-  `sc_a_gruppi` text,
+  `sc_a_gruppi` text DEFAULT NULL,
   `sc_valore_gruppi` decimal(6,2) DEFAULT NULL,
   `sc_da_data` date DEFAULT NULL,
   `sc_custom_cb` varchar(255) NULL,
@@ -484,7 +484,7 @@ CREATE TABLE `#__gg_unit` (
 -- ----------------------------
 -- Records of #__gg_unit
 -- ----------------------------
-INSERT INTO `#__gg_unit` VALUES ('1', 'Corsi', 'corsi', '', '0', '0', '1', '0', 'Accesso libero', '1', '0', '2019-11-14', '2019-11-14', null, 0, null);
+INSERT INTO `#__gg_unit` VALUES ('1', 'Corsi', 'corsi', '', '0', '0', '1', '0', 'Accesso libero', '1', '0', '2019-11-14', '2019-11-14', null, 0, null, 0, null, null, null, null, null, null, null, null, null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for `#__gg_unit_map`
