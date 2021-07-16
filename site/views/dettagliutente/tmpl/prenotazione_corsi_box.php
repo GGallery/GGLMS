66a695afa2c0eb978df62950a5a49523d174ddca
Luca <luca.gallo@gallerygroup.it>
2021-07-16 12:25:22 +0200

    wip

---
+++ site/views/dettagliutente/tmpl/prenotazione_corsi_box.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Luca
+ * Date: 31/03/2021
+ * Time: 18:20
+ */
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<div class="container">
+
+    <?php
+        echo outputHelper::get_box_details($this->box_corsi);
+    ?>
+
+</div>
