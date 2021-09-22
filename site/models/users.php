51665d378d478badae1d753df0e5a9f41fed0db5
Luca <luca.gallo@gallerygroup.it>
2021-09-22 14:48:30 +0200

    wip attivazione utenti

--- site/models/users.php
+++ site/models/users.php
@@ -1641,7 +1641,7 @@ class gglmsModelUsers extends JModelLegacy
                                      )
                                     VALUES (
                                         " . $this->_db->quote($user_id) . ",
-                                        " . $this->_db->quote($codice_fiscale) . "
+                                        " . $this->_db->quote(strtoupper($codice_fiscale)) . "
                                     )";
 
             $this->_db->setQuery($query);
