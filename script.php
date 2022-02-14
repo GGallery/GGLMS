66c8aedfa42899e024ee949eba21fda05e1a30e6
Salma <salma.rajid@ggallery.it>
2022-02-14 12:43:21 +0100

    fix report zoom

--- script.php
+++ script.php
@@ -37,7 +37,8 @@ class com_gglmsInstallerScript {
                                     "report_per_piattaforma.php",
                                     "load_corsi_from_xml.php",
                                     "fix_report_anagrafiche.php",
-                                    "fix_report_anagrafiche_2.php"
+                                    "fix_report_anagrafiche_2.php",
+                                    "get_event_log.php"
                                     );
 
             foreach ($cli_script_arr as $key_script => $cli_script) {
