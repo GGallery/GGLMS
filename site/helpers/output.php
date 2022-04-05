5b37d649b5af3300c412ec015e43b7a20b9be2bc
Luca <luca.gallo@gallerygroup.it>
2022-04-05 08:58:35 +0200

    wip

--- site/helpers/output.php
+++ site/helpers/output.php
@@ -1996,7 +1996,7 @@ HTML;
                         <td><h5>€ <b><span id="amount_span">{$_tariffa}</span></b></h5></td>
                     </tr>
                     <tr>
-                        <td colspan="5" style="text-center">
+                        <td colspan="5" class="text-center">
                             <span id="paypal-button-container"></span>
                         </td>
                     </tr>
