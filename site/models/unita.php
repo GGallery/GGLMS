465fe2977a5a2d287e76cfbbdbecc16114d5dd8e
Luca <luca.gallo@gallerygroup.it>
2022-09-16 18:45:00 +0200

    fix importa corsi xml error message

--- site/models/unita.php
+++ site/models/unita.php
@@ -963,7 +963,7 @@ class gglmsModelUnita extends JModelLegacy
 
             $this->_db->setQuery($insert);
             if (!$this->_db->execute())
-                throw new Exception("Inserimento corso fallito: " . $this->_db->getQuery()->dump(), E_USER_ERROR);
+                throw new Exception("Inserimento corso fallito: " . $this->_db->getErrorMsg(), E_USER_ERROR);
 
             $last_unit_id = $this->_db->insertid();
 
@@ -1007,7 +1007,7 @@ class gglmsModelUnita extends JModelLegacy
 
             $this->_db->setQuery($insert);
             if (!$this->_db->execute())
-                throw new Exception("Inserimento riferimento utente per corso fallito: " . $this->_db->getQuery()->dump(), E_USER_ERROR);
+                throw new Exception("Inserimento riferimento utente per corso fallito: " . $this->_db->getErrorMsg(), E_USER_ERROR);
 
             $this->_db->transactionCommit();
 
