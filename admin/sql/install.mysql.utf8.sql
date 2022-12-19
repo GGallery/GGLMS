4e591b9446bbc9aae9b2f81b45d5ed63b71cbb69
Luca <luca.gallo@gallerygroup.it>
2022-12-19 18:13:27 +0100

    wip custom sql install

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -813,6 +813,7 @@ CREATE TABLE `#__gg_anagrafica_centri` (
 -- Table structure for `#__gg_registration_request`
 -- ----------------------------
 
+DROP TABLE IF EXISTS `#__gg_registration_request`;
 CREATE TABLE `#__gg_registration_request` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` INT(11) NOT NULL,
