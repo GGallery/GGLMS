cd49c2bf7af4292b4c4d78a1163e598ad95f391e
Luca <luca.gallo@gallerygroup.it>
2023-03-14 17:12:58 +0100

    fix sql

--- admin/sql/updates/mysql/4.2.4.sql
+++ admin/sql/updates/mysql/4.2.4.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `#__gg_vendita_sconti_particolari`
-ADD COLUMN `sc_eta_valore` DECIMAL(6,2) NULL DEFAULT NULL COMMENT 'Attiva sconto per eta' AFTER `sc_data_valore`
+ADD COLUMN `sc_eta_valore` DECIMAL(6,2) NULL DEFAULT NULL COMMENT 'Attiva sconto per eta' AFTER `sc_data_valore`,
 ADD COLUMN `rif_eta` INT NULL DEFAULT NULL AFTER `sc_eta_valore`;
 
