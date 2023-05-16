0e67bd41e957d732fed447681668531b60a92283
Salma <salma.rajid@ggallery.it>
2023-05-16 15:54:38 +0200

    fix bug mysql

--- admin/sql/updates/mysql/3.8.44.sql
+++ admin/sql/updates/mysql/3.8.44.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `#__gg_contenuti`
-ADD COLUMN `attestato_path`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `path`;
+ADD COLUMN `attestato_path` varchar(255) DEFAULT NULL;
 update #__gg_contenuti set attestato_path=path;
