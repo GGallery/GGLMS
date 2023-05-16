1f9cfde2334c4367a1bef87a2492c26e155179a3
Salma <salma.rajid@ggallery.it>
2023-05-16 15:49:34 +0200

    fix bug mysql

--- admin/sql/updates/mysql/3.8.42.sql
+++ admin/sql/updates/mysql/3.8.42.sql
@@ -12,3 +12,31 @@ CREATE TABLE `#__gg_report_view_permessi_gruppi` (
 PRIMARY KEY (`id`, `id_utente`)
 )
 ;
+
+CREATE TABLE IF NOT EXISTS `#__gg_contenuti` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titolo` text NOT NULL,
+  `alias` text,
+  `pubblicato` tinyint(1) NOT NULL DEFAULT '1',
+  `durata` int(10) unsigned NOT NULL DEFAULT '0',
+  `descrizione` longtext,
+  `access` int(10) unsigned DEFAULT '0',
+  `meta_tag` varchar(255) DEFAULT '-',
+  `abstract` mediumtext,
+  `datapubblicazione` date DEFAULT NULL,
+  `slide` tinyint(1) NOT NULL DEFAULT '0',
+  `path` varchar(255) DEFAULT NULL,
+  `id_quizdeluxe` int(4) DEFAULT NULL,
+  `tipologia` int(4) NOT NULL DEFAULT '1',
+  `files` varchar(255) DEFAULT NULL COMMENT 'NON CANCELLARE SERVE PER IL MAP',
+  `mod_track` int(255) DEFAULT '1',
+  `prerequisiti` varchar(255) DEFAULT NULL,
+  `id_completed_data` int(10) DEFAULT NULL,
+  `attestato_path` varchar(255) DEFAULT NULL,
+  `orientamento` varchar(1) DEFAULT NULL,
+  `path_pdf` varchar(255) DEFAULT NULL COMMENT 'Integrazione per migrazione da vecchio GGLMS',
+  `id_evento` varchar(25) NULL COMMENT 'aggiunta per le chiamate api zoom',
+  `tipo_zoom` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'aggiunta per distinguere webinar da meeting',
+  `url_streaming_azure` TEXT DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
