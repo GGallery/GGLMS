bac67ed357e790bbbfd8d3d1be3bf482bc8dd0be
Geovanny Alvarado <geovanny@ggallery.it>
2024-07-24 14:19:54 +0200

    wip cli

---
+++ extra/upload_media_to_s3.php
@@ -0,0 +1,66 @@
+<?php
+define('_JEXEC', 1);
+
+error_reporting(E_ALL | E_NOTICE);
+ini_set('display_errors', 1);
+define('JDEBUG', 1);
+
+// Load system defines
+if (file_exists(dirname(__DIR__) . '/defines.php'))
+{
+    require_once dirname(__DIR__) . '/defines.php';
+}
+
+if (!defined('_JDEFINES'))
+{
+    define('JPATH_BASE', dirname(__DIR__));
+    require_once JPATH_BASE . '/includes/defines.php';
+}
+
+if (!defined('JPATH_COMPONENT')) {
+    define('JPATH_COMPONENT', JPATH_SITE . '/components/com_gglms');
+}
+
+require_once JPATH_LIBRARIES . '/import.legacy.php';
+require_once JPATH_LIBRARIES . '/cms.php';
+
+// Load the configuration
+require_once JPATH_CONFIGURATION . '/configuration.php';
+
+require_once JPATH_SITE . '/administrator/components/com_gglms/models/libs/debugg/debugg.php';
+require_once JPATH_COMPONENT . '/helpers/output.php';
+require_once JPATH_COMPONENT . '/helpers/utility.php';
+
+class Upload_media_to_s3 extends JApplicationCli {
+    public function doExecute(){
+
+        try{
+        $db_host = $this->input->get('db_host', 'localhost');
+        $db_port = $this->input->get('db_port', '3306');
+        $db_user = $this->input->get('db_user', null);
+        $db_password = $this->input->get('db_password', null);
+        $db_database = $this->input->get('db_database', null);
+        $db_prefix = $this->input->get('db_prefix', null);
+        $db_driver = $this->input->get('db_driver', null);
+
+        require_once JPATH_COMPONENT . '/controllers/aws.php';
+
+        $this->out('Script start at:' . date('H:i:s') . ' on ' . date('d/m/Y'));
+
+        $aws = new gglmsControllerAws();
+
+        $upload_media = $aws->loadContents($db_host . ":" . $db_port,
+                                                                    $db_user,
+                                                                    $db_password,
+                                                                    $db_database,
+                                                                    $db_prefix,
+                                                                    $db_driver);
+            $this->out('Script ended with ' . $importa_master_farmacie . ' at:' . date('H:i:s') . ' on ' . date('d/m/Y'));
+        }
+        catch (Exception $e) {
+            $this->out(date('d/m/Y H:i:s') . ' - ERRORE: ' . $e->getMessage());
+        }
+
+    }
+}
+JApplicationCli::getInstance('Upload_media_to_s3')->execute();
--- site/controllers/aws.php
+++ site/controllers/aws.php
@@ -10,14 +10,13 @@ use Aws\Exception\AwsException;
 
 class gglmsControllerAws extends JControllerLegacy
 {
-
+    protected $_db;
+    protected $_config;
     protected $acces_id;
     protected $secret_key;
     protected $region;
-
     protected $s3Client;
     protected $bucket;
-    
     protected $site_token;
     
 
@@ -61,14 +60,36 @@ class gglmsControllerAws extends JControllerLegacy
     }
 
 
-    public function loadContents()
-    {
-
+    public function loadContents($db_host = null,
+                                            $db_user = null,
+                                            $db_password = null,
+                                            $db_database = null,
+                                            $db_prefix = null,
+                                            $db_driver = null,){
+        $exists_check = array();
+        $db_option = array();
         try {
+            if (!is_null($db_host)||!is_null($db_driver)) {
+
+                $db_option['driver'] = $db_driver;
+                $db_option['host'] = $db_host;
+                $db_option['user'] = $db_user;
+                $db_option['password'] = utilityHelper::encrypt_decrypt('decrypt', $db_password, "GGallery00!", "GGallery00!");
+                $db_option['database'] = $db_database;
+                $db_option['prefix'] = $db_prefix;
+
+                $this->_db = JDatabaseDriver::getInstance($db_option);
+                $this->_config = new gglmsModelConfig();
+                $this->site_token = $this->_config->getConfigValue('aws_token');
+            }
+
             $this->s3Client->uploadDirectory('../mediagg', $this->bucket.'/'.$this->site_token.'/mediagg');
+            echo "Directory uploaded successfully.\n";
+            return 1;
 
         } catch (S3Exception $e) {
             echo $e->getMessage();
+            return 0;
         }
 
     }
