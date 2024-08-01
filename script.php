4a78f315989c78ee3f2b9eb1e2f189149634cf15
Geovanny Alvarado <geovanny@ggallery.it>
2024-08-01 15:33:48 +0200

    agigunto script

--- script.php
+++ script.php
@@ -43,6 +43,7 @@ class com_gglmsInstallerScript {
                                     "import_anagrafica_farmacie.php",
                                     "report_farmacie.php",
                                     "allinea_usergroup_utenti.php",
+                                    "report_farmacie_requests.php",
                                     );
 
             foreach ($cli_script_arr as $key_script => $cli_script) {
