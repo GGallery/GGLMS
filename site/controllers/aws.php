3536d1a3c4016c212f4d6e4eab2856098da39c1e
Geovanny Alvarado <geovanny@ggallery.it>
2025-01-23 15:17:01 +0100

    fix

--- site/controllers/aws.php
+++ site/controllers/aws.php
@@ -184,7 +184,7 @@ class gglmsControllerAws extends JControllerLegacy
     }
 
     public function getAwsMediaUrl(){
-        $s3Url = $this->bucketEndpoint .'/'.$this->bucket.'/mediagg'.'/'.$this->site_token;
+        $s3Url = $this->bucketEndpoint .'/'.$this->bucket.'/'.$this->site_token;  //../mediagg ecc...
 
         return $s3Url;
     }
