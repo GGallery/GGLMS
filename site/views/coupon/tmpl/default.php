9153531f0835998f2b7662b3cc967cbbb21714cb
Luca Gallo <luca.gallo@gallerygroup.it>
2026-05-21 17:07:23 +0200

    wip ausind migration

--- site/views/coupon/tmpl/default.php
+++ site/views/coupon/tmpl/default.php
@@ -7,7 +7,8 @@ defined('_JEXEC') or die('Restricted access');
 <script>
     jQuery(function () {
         jQuery.ajaxSetup({cache: false});
-        jQuery("button").click(function (e) {
+        //jQuery("button").click(function (e) {
+        jQuery("#button_conferma_codice").click(function (e) {
             e.preventDefault();
             jQuery("#button_conferma_codice").hide();
             jQuery("#waiting_verifica_codice").show();
@@ -30,8 +31,9 @@ defined('_JEXEC') or die('Restricted access');
 </script>
 
 
-<div id="box_coupon_container">
+<div id="box_coupon_container" <?php echo $this->hideCouponGeneration ? 'style="height: inherit !important;"' : ''; ?>>
     <div id="box_coupon">
+        <?php if (!$this->hideCouponGeneration) : ?>
         <h3><?php echo JText::_('COM_GGLMS_COUPON_INSERT'); ?></h3>
         <p><?php echo JText::_('COM_GGLMS_COUPON_DESCRIZIONE'); ?></p>
         <p>
@@ -42,6 +44,33 @@ defined('_JEXEC') or die('Restricted access');
         <div id="waiting_verifica_codice" class="hide">
             <h3><?php echo JText::_('COM_GGLMS_COUPON_VERIFICA'); ?></h3>
         </div>
+        <? else: ?>
+        <h3><b>La piattaforma si rinnova!</b></h3>
+        <p>La versione attuale della piattaforma resterà disponibile ancora per alcuni mesi come archivio dei corsi già conclusi, ma non sarà più possibile attivare nuovi corsi.</p>
+        <p>Il tuo account è già stato trasferito alla nuova piattaforma.</p>
+        <h4><b>Come accedere?</b></h4>
+        <ol>
+            <li>Clicca sul pulsante qui sotto e utilizza il box <b>ACCEDI</b>.  Non creare un nuovo account!</li>
+            <li>Inserisci l'indirizzo e-mail associato al tuo account sia nel campo <b>USERNAME</b> che nel campo <b>PASSWORD</b></li>
+            <li>Riceverai una e-mail con un link per riattivare il tuo account e impostare una nuova password
+                <br /><i>Per motivi di privacy, la tua password attuale non è stata trasferita. Potrai scegliere se impostarne una nuova oppure utilizzare la stessa che avevi in precedenza.</i>
+            </li>
+            <li>Una volta effettuato l'accesso, vai sulla scheda <b>CATALOGO CORSI</b> e troverai il box dedicato all'inserimento del coupon per sbloccare l'iscrizione al corso e iniziare la fruizione</li>
+        </ol>
+        <h4><b>E per i successivi accessi?</b></h4>
+        <p>Trovi il link della nuova piattaforma in home page!</p>
+        <h4><b>Hai bisogno di aiuto?</b></h4>
+        <p>Utilizza il pulsante rosso di assistenza che trovi nell’angolo in basso a destra della nuova piattaforma.</p>
+        <p style="text-align: center">
+            <button 
+                class="btn btn-primary btn-lg"
+                style="height: 50px !important; width: 200px; border-radius: 7px !important;"
+                onclick="javascript:event.preventDefault();window.open('<?php echo $this->newPlatformUrl; ?>', '_blank')"
+                >
+              <b>ACCEDI</b>
+            </button>
+        </p>
+        <? endif; ?>
     </div>
 
     <div id="report"></div>
--- site/views/coupon/view.html.php
+++ site/views/coupon/view.html.php
@@ -20,6 +20,9 @@ jimport('joomla.application.component.helper');
 class gglmsViewCoupon extends JViewLegacy {
 
     protected $params;
+    protected string $currentUrl;
+    protected bool $hideCouponGeneration;
+    protected string $newPlatformUrl;
 
     function display($tpl = null)
     {
@@ -29,6 +32,32 @@ class gglmsViewCoupon extends JViewLegacy {
 
         $this->_params = $this->coupon->_params;
 
+        // current URL
+        $blackListedAddresses = [
+            'csifad.confindustriacuneo.it', 
+            'fad.assoservizilegnano.it', 
+            'serviziconfindustriavarese.ausindfad.it',
+            //'test.gallerygroup.dvl.to',
+        ];
+        $urlPairs = [
+            'web.ausindfad.it' => 'https://training.ausindfad.it',
+            'web.assolombardaservizifad.it' => 'https://web2.assolombardaservizifad.it',
+            'csifad.confindustriacuneo.it' => 'https://csi-elearning.confindustriacuneo.it',
+            'fad.assoservizilegnano.it' => 'https://elearning.assoservizilegnano.it',
+            'serviziconfindustriavarese.ausindfad.it' => 'https://scv.ausindfad.it',
+            //'test.gallerygroup.dvl.to' => 'https://nasa.gov',
+        ];
+        $this->currentUrl = $_SERVER['HTTP_HOST'];
+        // verifico se l'indirizzo corrente è blacklisted
+        $this->hideCouponGeneration = in_array(
+            $this->currentUrl,
+            array_map('strtolower', $blackListedAddresses),
+            true
+        );
+
+        if ($this->hideCouponGeneration) {
+            $this->newPlatformUrl = $urlPairs[$this->currentUrl];
+        }
 
         parent::display($tpl);
     }
